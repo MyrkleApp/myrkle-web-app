c8fd645718bc3a6ef6204ab80f464e15007ab8cc
Sammy <sammyoziegbe@gmail.com>
2024-03-21 17:08:17 +0100

    navigate to wallet page from "/"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Wallet from "./features/wallet/pages";
 import Transaction from "./features/transactions/pages";
 import NftGallery from "./features/wallet/pages/nft-gallery";
@@ -15,7 +15,6 @@ import Flags from "./features/terminal/pages/flags";
 import Exchange from "./features/exchange/pages";
 import Escrows from "./features/terminal/pages/escrows";
 import BurnAsset from "./features/terminal/pages/burn-asset";
-import Auth from "./pages/auth";
 import Settings from "./features/settings/pages";
 import useRehydrateSignInData from "./features/auth/hooks/use-rehydrate-signin-data";
 import useRetrieveWallets from "./features/wallet/hooks/use-retrieve-wallets";
@@ -43,8 +42,8 @@ function App() {
     <Flex justify="center" align="center" bg="#000000" h="100vh" w="100vw">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate replace to={ROUTES.WALLET} />} />
           <Route path={ROUTES.LANDING} element={<LandingPage />} />
-          <Route path={ROUTES.AUTH} element={<Auth />} />
           <Route path={ROUTES.WALLET} element={<Wallet />} />
           <Route path={ROUTES.WALLET_NFT_GALLERY} element={<NftGallery />} />
           <Route path={ROUTES.WALLET_NFT_DETAIL} element={<NftDetail />} />
